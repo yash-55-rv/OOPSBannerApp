532a6c7310d790cd71bad4f34cea204c8a28e050
yash-55-rv <yashwanthrachamalla2007@gmail.com>
2026-02-28 16:58:10 +0530

    use case 3

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,11 +1,11 @@
 public class OopsBannerApp {
     public static void main(String[] args) throws Exception {
-        System.out.println("  *****     *****    ******     ****** ");
-        System.out.println(" *     *   *     *   *     *    *     ");
-        System.out.println("*       * *       *  *     *    *      ");
-        System.out.println("*       * *       *  ******     ****** ");
-        System.out.println("*       * *       *  *               * ");
-        System.out.println(" *     *   *     *   *               *  ");
-        System.out.println("  *****     *****    *          ******   ");
+        System.out.println(String.join("   ","  *****  ","  *****  "," ******  ","  ****** "));
+        System.out.println(String.join("   "," *     * "," *     * "," *     * ","  *      "));
+        System.out.println(String.join("   ","*       *","*       *"," *     * ","  *      "));
+        System.out.println(String.join("   ","*       *","*       *"," ******  ","  ****** "));
+        System.out.println(String.join("   ","*       *","*       *"," *       ","       * "));
+        System.out.println(String.join("   "," *     * "," *     * "," *       ","       * "));
+        System.out.println(String.join("   ","  *****  ","  *****  "," *       ","  ****** "));
     }
 }
