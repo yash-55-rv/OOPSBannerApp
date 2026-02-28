32fb17a8823252637ccc84011bef50ac02bb9aaf
yash-55-rv <yashwanthrachamalla2007@gmail.com>
2026-02-28 20:51:33 +0530

    Use case 5

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,13 +1,15 @@
 public class OopsBannerApp {
-    public static void main(String[] args) throws Exception {
-        String[] lines = new String[7];
-        lines[0] = String.join("   ","  *****  ","  *****  "," ******  ","  ****** ");
-        lines[1] = String.join("   "," *     * "," *     * "," *     * ","  *      ");
-        lines[2] = String.join("   ","*       *","*       *"," *     * ","  *      ");
-        lines[3] = String.join("   ","*       *","*       *"," ******  ","  ****** ");
-        lines[4] = String.join("   ","*       *","*       *"," *       ","       * ");
-        lines[5] = String.join("   "," *     * "," *     * "," *       ","       * ");
-        lines[6] = String.join("   ","  *****  ","  *****  "," *       ","  ****** ");
+    public static void main(String[] args)  {
+        String[] lines = {
+            String.join("   ","  *****  ","  *****  "," ******  ","  ****** "),
+            String.join("   "," *     * "," *     * "," *     * ","  *      "),
+            String.join("   ","*       *","*       *"," *     * ","  *      "),
+            String.join("   ","*       *","*       *"," ******  ","  ****** "),
+            String.join("   ","*       *","*       *"," *       ","       * "),
+            String.join("   "," *     * "," *     * "," *       ","       * "),
+            String.join("   ","  *****  ","  *****  "," *       ","  ****** "),
+        };
+        
         for (String line : lines) {
             System.out.println(line);
         }
