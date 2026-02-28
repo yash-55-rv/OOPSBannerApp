c03b3360f015a78162850e3f5f468b4856522714
yash-55-rv <yashwanthrachamalla2007@gmail.com>
2026-02-28 21:46:59 +0530

    Use case 6

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,17 +1,45 @@
 public class OopsBannerApp {
-    public static void main(String[] args)  {
-        String[] lines = {
-            String.join("   ","  *****  ","  *****  "," ******  ","  ****** "),
-            String.join("   "," *     * "," *     * "," *     * ","  *      "),
-            String.join("   ","*       *","*       *"," *     * ","  *      "),
-            String.join("   ","*       *","*       *"," ******  ","  ****** "),
-            String.join("   ","*       *","*       *"," *       ","       * "),
-            String.join("   "," *     * "," *     * "," *       ","       * "),
-            String.join("   ","  *****  ","  *****  "," *       ","  ****** "),
+    public static String[] getOpattern(){
+        return new String[] {
+            "  *****  ",
+            " *     * ",
+            "*       *",
+            "*       *",
+            "*       *",
+            " *     * ",
+            "  *****  "
+
         };
+    }
+    public static String[] getPPattern() {
+        return new String[] {
+            " ******  ",
+            " *     * ",
+            " *     * ",
+            " ******  ",
+            " *       ",
+            " *       ",
+            " *       "
+        };
+    }
+    public static String[] getSPattern() {
+        return new String[] {
+            "  ****** ",
+            "  *      ",
+            "  *      ",
+            "  ****** ",
+            "       * ",
+            "       * ",
+            "  ****** "
+        };
+    }
+    public static void main(String[] args)  {
+        String[] opattern = getOpattern();
+        String[] ppattern = getPPattern();
+        String[] spattern = getSPattern();
         
-        for (String line : lines) {
-            System.out.println(line);
+        for (int i = 0; i < opattern.length; i++) {
+            System.out.println(opattern[i] + "  " + opattern[i] + "  " + ppattern[i] + "  " + spattern[i]);
         }
     }
 }
